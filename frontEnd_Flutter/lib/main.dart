cbd21f2fa0db046d1fa6e67c1f86a423b0f498f9
sardaarNiamotullah <sardaar.niamotullah@gmail.com>
2025-07-26 23:41:49 +0600

    refactor: remove unnecessary comments for cleaner code

--- frontEnd_Flutter/lib/main.dart
+++ frontEnd_Flutter/lib/main.dart
@@ -1,24 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'screens/landing_page.dart';
-//
-// void main() {
-//   runApp(MyApp());
-// }
-//
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false, // 👈 Hides the DEBUG banner
-//       title: 'ToDoS',
-//       theme: ThemeData(
-//         primarySwatch: Colors.blue,
-//       ),
-//       home: LandingPage(),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart'; // ✅ Import GetX
 import 'screens/landing_page.dart';
